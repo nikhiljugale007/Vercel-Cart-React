2cf01d455a647c62b64cbc94e750b25504f67a94
Nikhil Jugale <nikhiljugale007@gmail.com>
2022-03-21 17:14:44 +0530

    added common images

---
+++ src/assets/index.js
@@ -0,0 +1,12 @@
+export const hero1 = require("./hero1.jpeg");
+export const casualshoes = require("./casual-shoes.webp");
+export const logo = require("./ecommerce-logo.png");
+export const flipflop = require("./flip-flop.webp");
+export const formalshoes = require("./formal-shoes.webp");
+export const hrxlogo = require("./hrx-logo.png");
+export const nikelogo = require("./nike-logo.jpg");
+export const sandal = require("./sandal.webp");
+export const woodlandlogo = require("./woodland.jpeg");
+export const wrognlogo = require("./wrogn-logo.jpg");
+export const sportshoes = require("./sport_shoes.jpg");
+export const sneakers = require("./sneakers.webp");
