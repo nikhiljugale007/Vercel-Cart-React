7ad4127e20803446d481d52422a0f927f3ba511f
Nikhil Jugale <nikhiljugale007@gmail.com>
2022-03-22 08:39:47 +0530

    added header component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Mockman from "mockman-js";
 import { Home } from "./pages";
+import { Header } from "./components";
 function App() {
 	return (
 		<>
+			<Header />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/mockman" element={<Mockman />} />
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,60 @@
+import { React } from "react";
+import { logo } from "../../assets";
+import { Link } from "react-router-dom";
+import "./Header.css";
+const Header = () => {
+	return (
+		<div className="header-container">
+			<header className="header">
+				<div className="header-sub-container">
+					<button className="btn btn-primary btn-icon btn-menu">
+						<i className="fa fa-bars fa-lg" aria-hidden="true"></i>
+					</button>
+					<Link to="/home" className="btn btn-link btn-hide header-link">
+						<img className="img-round logo-container" src={logo} />
+					</Link>
+					<Link to="/home" className="btn btn-link header-link">
+						Home
+					</Link>
+					<Link to="/product" className="btn btn-link btn-hide header-link">
+						Shop
+					</Link>
+				</div>
+				<div className="search-container btn-hide">
+					<button className="btn btn-icon">
+						<i className="fa fa-search fa-lg" aria-hidden="true"></i>
+					</button>
+					<input
+						type="text"
+						className="search-bar"
+						placeholder="woodland shoes.."
+					/>
+				</div>
+
+				<div className="header-sub-container">
+					<Link to="/wishlist" className="link-no-style">
+						<button className="btn btn-primary btn-icon btn-hide">
+							<i className="far fa-heart fa-2x badge-parent">
+								<span className="badge-content badge-itself">5</span>
+							</i>
+						</button>
+					</Link>
+					<Link to="/cart" className="link-no-style">
+						<button className="btn btn-primary btn-icon btn-hide">
+							<i className="fas fa-shopping-basket fa-2x badge-parent">
+								<span className="badge-content badge-itself">3</span>
+							</i>
+						</button>
+					</Link>
+					<Link to="/" className="link-no-style">
+						<button className="btn btn-primary btn-icon">
+							<i className="far fa-user-circle fa-2x"></i>
+						</button>
+					</Link>
+				</div>
+			</header>
+		</div>
+	);
+};
+
+export { Header };
