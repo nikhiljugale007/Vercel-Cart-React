2f150b64c600a4da4e0441909e4ee371f6b78967
Nikhil Jugale <nikhiljugale007@gmail.com>
2022-03-26 15:19:56 +0530

    removed unsed variable which is failing netlify deploy

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import { logo } from "../../assets";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Header.css";
 import { useProductContext } from "../../context/ProductContext";
 const Header = () => {
