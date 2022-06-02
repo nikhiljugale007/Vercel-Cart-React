46772553d697c3dd77782a66ba591607191f13a9
nikhiljugale007 <nikhiljugale007@gmail.com>
2022-06-02 08:38:35 +0530

    added favicon and title

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import Mockman from "mockman-js";
 import { Home, Product, Cart, Wishlist, Login, SignUp, Profile } from "./pages";
 import { Header } from "./components";
 function App() {
-	return (
-		<>
-			<Header />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/product" element={<Product />} />
-				<Route path="/cart" element={<Cart />} />
-				<Route path="/mockman" element={<Mockman />} />
-				<Route path="/wishlist" element={<Wishlist />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/signup" element={<SignUp />} />
-				<Route path="/profile" element={<Profile />} />
-			</Routes>
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/mockman" element={<Mockman />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
