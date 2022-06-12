057f3f07b9febabadee6eb65b77eb4086ae44ca1
nikhiljugale007 <nikhiljugale007@gmail.com>
2022-06-12 10:33:06 +0530

    minor  change

--- src/components/auth_wrapper/RequireAuth.jsx
+++ src/components/auth_wrapper/RequireAuth.jsx
@@ -2,10 +2,8 @@ import { Navigate } from "react-router-dom";
 import { useAuthContext } from "../../context/AuthContext";
 const RequireAuth = ({ children, from }) => {
   const { authState } = useAuthContext();
-  if (localStorage.getItem("token")) {
-    if (!authState.isLoggedIn) {
-      localStorage.removeItem("token");
-    }
+  if (localStorage.getItem("token") && !authState.isLoggedIn) {
+    localStorage.removeItem("token");
   }
   return authState.isLoggedIn ? (
     children
